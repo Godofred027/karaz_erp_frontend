2d3dc993d268874ade9ef60008b6c230a83198ac
Andres Paredes <godofredo.24.p@gmail.com>
2023-01-17 17:33:03 -0500

    frontend upload - dashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "next/app";
+import Head from "next/head";
+import Router from "next/router";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/styles/globals.css";
+import "@/styles/tailwind.css";
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+    return (
+      <React.Fragment>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
